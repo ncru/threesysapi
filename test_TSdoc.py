12c0c3fc60afd608af1bfb316df994ef6bc4ad3a
CodeDFranky <don.franco.ramos@adamson.edu.ph>
2022-12-30 18:57:34 +0800

    started unit test writing

---
+++ test_TSdoc.py
@@ -0,0 +1,12 @@
+import unittest
+from modules.TSdoc import TSdoc
+
+
+class TestTSdoc(unittest.TestCase):
+
+    def test_TSdoc_initialization(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
